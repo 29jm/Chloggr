65127ba723a7086ff243bdd44129173445ae2921
gabs1234 <gabslefloch@gmail.com>
2014-08-06 17:12:43 +0200

    removed minutes and hours counter

--- js/timer.js
+++ js/timer.js
@@ -1,45 +1,26 @@
 var seconds = 0;
-var minutes = 0;
-var hours = 0;
 var intervalSeconds;
 var timerIsOn = 0;
 
-function timeCount()
-{
-   seconds = seconds + 1;
+function timeCount() {
+   seconds++;
    intervalSeconds = setTimeout("timeCount()", 1000);
-
-   if (seconds >= 60)
-   {
-      minutes = Math.floor (seconds / 60) + minutes;
-      seconds %= 60;
-   }
-   if (minutes >= 60)
-   {
-      hours = Math.floor (minutes / 60) + hours;
-      minutes %= 60;
-   }
 }
 
-function playTimer() // Activates when user clicks on the "Play" button.
-{
-   if (!timerIsOn)// Check if the user clicks one time on the "Play" button-to avoid many timers at the same time.
-   {
+function playTimer() {
+   if (!timerIsOn) {
       timerIsOn = 1;
       timeCount();
    }
 }
 
-function stopTimer()//activates when user clicks on the "Pause" button.
-{
+function stopTimer() {
    clearTimeout(intervalSeconds);
    timerIsOn = 0;
 }
-function resetTimer()
-{
+
+function resetTimer() {
    seconds = 0;
-   minutes = 0;
-   hours = 0;
    intervalSeconds;
    timerIsOn = 0;
    playTimer();
