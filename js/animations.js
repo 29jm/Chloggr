befe8b55a41a99e567a8667b2fcfc1d960fce79b
gabs1234 <gabslefloch@gmail.com>
2014-08-12 22:43:05 +0200

    added animations.js for velocity.js animations

---
+++ js/animations.js
@@ -0,0 +1,28 @@
+var marginLeft = window.innerWidth/2 - 80;
+var marginTop = window.innerHeight-70;
+
+function pauseAnimation(){
+  	$( ".pause" ).velocity({
+    	width: "10em",
+    	height: "10em",
+    	top: "7em",
+    	translateX: [0, -700],
+    	borderRadius: "50%",
+    	opacity: "1",
+  		}, 900, [600, 25]).removeClass("pause").addClass("play");
+
+  	$( ".options" ).velocity('transition.fadeIn');
+}
+
+function playAnimation() {
+	$( ".play" ).velocity({
+		width: "70px",
+    	height: "70px",
+    	top: marginTop,
+    	translateX: [0, 700],
+      borderBottomLeftRadius: "0",
+      opacity: "0.5",
+		}, 900, [600, 25]).addClass("pause").removeClass("play");
+
+	$( ".options" ).velocity('transition.fadeOut');
+}
